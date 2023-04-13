15216b5acbf919c8ea2610d7e8b0a2219dddcacc
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2023-04-12 22:36:40 -0700

    created file structure, set up log-in/create account page.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Get the 'login' button element by its ID and add a click event listener
+document.getElementById('login').addEventListener('click', () => {
+    alert('Log In button clicked!');
+  });
+  
+  // Get the 'create-account' button element by its ID and add a click event listener
+  document.getElementById('create-account').addEventListener('click', () => {
+    alert('Create Account button clicked!');
+  });
+  
