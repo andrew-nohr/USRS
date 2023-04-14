e18f8993025b10d3fa6072122632e650551cd84f
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2023-04-14 15:34:21 -0700

    working on getting the create button to take us to the profile page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,5 @@
-import { auth } from "./firebase-init.js";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBsjXx0ax8Qh3b8_f6PmN0ARHThgXrR6dg",
-    authDomain: "usrs-99501.firebaseapp.com",
-    projectId: "usrs-99501",
-    storageBucket: "usrs-99501.appspot.com",
-    messagingSenderId: "410770603376",
-    appId: "1:410770603376:web:0b7c2fe48f76b3834923b5",
-    measurementId: "G-H970355Y3Z"
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth(app);
   
   // Navigate to Create Account page
   document.getElementById("create-account").addEventListener("click", function () {
     window.location.href = "create-account.html";
   });
-  
-  // User registration for Create Account page
-  const createAccountForm = document.getElementById("create-account-form");
-  
-  if (createAccountForm) {
-    createAccountForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-  
-      const username = document.getElementById("username").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      const confirmPassword = document.getElementById("confirm-password").value;
-  
-      if (password !== confirmPassword) {
-        alert("Passwords do not match.");
-        return;
-      }
-  
-      try {
-        const userCredential = await auth.createUserWithEmailAndPassword(email, password);
-        const user = userCredential.user;
-  
-        // TODO: Save the username to the user's profile or a separate database collection
-  
-        alert("Account created successfully!");
-        // Redirect to the user profile page
-        window.location.href = "profile.html";
-      } catch (error) {
-        console.error("Error creating user:", error);
-        alert("Error creating account: " + error.message);
-      }
-    });
-  }
-  
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,29 +1,4 @@
-// Firebase configuration and initialization
-var firebaseConfig = {
-    apiKey: "AIzaSyBsjXx0ax8Qh3b8_f6PmN0ARHThgXrR6dg",
-    authDomain: "usrs-99501.firebaseapp.com",
-    projectId: "usrs-99501",
-    storageBucket: "usrs-99501.appspot.com",
-    messagingSenderId: "410770603376",
-    appId: "1:410770603376:web:0b7c2fe48f76b3834923b5",
-    measurementId: "G-H970355Y3Z"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
-  // Set up Firestore and Auth references
-  const db = firebase.firestore();
-  const auth = firebase.auth();
-  
-  // Google Maps initialization function
-  function initMap() {
-    const mapOptions = {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    };
-    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }
-  
-
+// Google Maps initialization function
 function initMap() {
     const mapOptions = {
       center: { lat: -34.397, lng: 150.644 },
@@ -32,7 +7,6 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), mapOptions);
   }
   
-  
   // Function to handle clicks on the profile image
   function onProfileImageClick() {
     // Add code for handling the dropdown menu
@@ -40,8 +14,8 @@ function initMap() {
   }
   
   // Add event listener for the profile image 
-  const profileImage = document.querySelector(".profile-image");
-  profileImage.addEventListener("click", onProfileImageClick);
+  // const profileImage = document.querySelector(".profile-image");
+  // profileImage.addEventListener("click", onProfileImageClick);
   
   // Add event listeners for bottom navbar icons
   const bottomNavbarIcons = document.querySelectorAll(".bottom-navbar img");
@@ -50,4 +24,7 @@ function initMap() {
       // Add code here to handle the bottom navbar icons' click events
     });
   });
+  
+  // Initialize the Google Map
+  initMap();
   
