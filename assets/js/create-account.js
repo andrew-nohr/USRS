92648189e76a8ed6cc18c4cd98112d09f977b1d2
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2023-04-14 23:50:49 -0700

    updated

--- assets/js/create-account.js
+++ assets/js/create-account.js
@@ -1,45 +1,45 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBsjXx0ax8Qh3b8_f6PmN0ARHThgXrR6dg",
-  authDomain: "usrs-99501.firebaseapp.com",
-  projectId: "usrs-99501",
-  storageBucket: "usrs-99501.appspot.com",
-  messagingSenderId: "410770603376",
-  appId: "1:410770603376:web:0b7c2fe48f76b3834923b5",
-  measurementId: "G-H970355Y3Z"
-};
+// Import 'auth' and 'createUser' from the 'firebase-init.js' file
+import { auth, createUser } from "./firebase-init.js";
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth(app);
+// Log the 'auth' object and the 'createUser' function to the console for debugging purposes
+console.log("auth object:", auth);
+console.log("createUser function:", createUser);
 
-// User registration for Create Account page
+// Get a reference to the create account form element
 const createAccountForm = document.getElementById("create-account-form");
 
+// Check if the form element exists
 if (createAccountForm) {
+  // Add a submit event listener to the form
   createAccountForm.addEventListener("submit", async (event) => {
+    // Prevent the default form submission behavior
     event.preventDefault();
 
+    // Get the input values from the form
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
 
+    // Check if the passwords match
     if (password !== confirmPassword) {
       alert("Passwords do not match.");
       return;
     }
 
+    // Try to create a new user account with the provided email and password
     try {
-      const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+      const userCredential = await createUser(auth, email, password);
       const user = userCredential.user;
 
       // TODO: Save the username to the user's profile or a separate database collection
 
+      // Show a success message
       alert("Account created successfully!");
       // Redirect to the user profile page
       window.location.href = "profile.html";
     } catch (error) {
+      // Log the error and show an error message
       console.error("Error creating user:", error);
       alert("Error creating account: " + error.message);
     }
